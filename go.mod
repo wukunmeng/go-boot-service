6850a0ea338179180191f876beba4f002c8b1151
wukunmeng <wukunmeng@xinpianchang.com>
2018-12-13 14:39:04 +0800

    初始化mod,并添加main

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/wukunmeng/go-boot-service
---
+++ version/version.go
@@ -0,0 +1,17 @@
+/**
+ * Create with IntelliJ IDEA
+ * Project name : go-boot-service
+ * Package name : 
+ * Author : Wukunmeng
+ * User : wukm
+ * Date : 18-12-13
+ * Time : 下午2:28
+ * ---------------------------------
+ * 
+ */
+package version
+
+var(
+    Build = "dev"
+    BuildTime = ""
+)
